592913a7a9c7dd04fee7c9e54b727964ca871457
Tim Ermilov <yamalight@gmail.com>
2016-07-04 14:31:51 +0200

    basic version that connects to rabbitmq and exposes messages through REST interface

---
+++ Makefile
@@ -0,0 +1,13 @@
+default: run
+
+run:
+	go run main.go
+
+test:
+	echo '{"route": "exynize.test", "data": {"test": "json"}}' | http POST localhost:8080
+
+rabbit:
+	docker run -d -p 5672:5672 -p 8081:15672 --name exynize-rabbit rabbitmq:management
+
+stop-rabbit:
+	docker stop exynize-rabbit
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/streadway/amqp"
+)
+
+var (
+	host             = "amqp://localhost:5672/"
+	exchange         = "exynize"
+	routingKey       = "exynize.test"
+	responseEndpoint = "http://localhost:3000/"
+)
+
+// Response is a structure for sending response from proxy to consumer
+type Response struct {
+	Body    interface{} `json:"body"`    // body of the message
+	ReplyTo string      `json:"replyTo"` // replyto param from rabbitmq
+	Route   string      `json:"route"`   // route param from rabbit
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial(host)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		exchange, // name
+		"topic",  // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		true,  // durable
+		true,  // delete when usused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	log.Printf("Binding queue %s to exchange %s with routing key %s", q.Name, exchange, routingKey)
+	err = ch.QueueBind(
+		q.Name,     // queue name
+		routingKey, // routing key
+		exchange,   // exchange
+		false,
+		nil)
+	failOnError(err, "Failed to bind a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto ack
+		false,  // exclusive
+		false,  // no local
+		false,  // no wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			decoder := json.NewDecoder(r.Body)
+			var data map[string]interface{}
+			err = decoder.Decode(&data)
+			if err != nil {
+				log.Printf("Couldn't read request body")
+				return
+			}
+			log.Printf("Got request body: %s", data)
+			route := data["route"].(string)
+			dataBody := data["data"].(interface{})
+			body, errMarshal := json.Marshal(dataBody)
+			if errMarshal != nil {
+				log.Printf("Couldn't marshal body")
+				return
+			}
+			err = ch.Publish(
+				exchange, // exchange
+				route,    // routing key
+				false,    // mandatory
+				false,    // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(body),
+				})
+			failOnError(err, "Failed to publish a message")
+			log.Printf(" [x] Sent %s", body)
+		})
+		err = http.ListenAndServe(":8080", nil)
+		failOnError(err, "Failed to start a server")
+	}()
+
+	go func() {
+		for d := range msgs {
+			var body map[string]interface{}
+			err = json.Unmarshal(d.Body, &body)
+			if err != nil {
+				log.Printf("Couldn't decode message body")
+				return
+			}
+			replyTo := d.ReplyTo
+			route := d.RoutingKey
+			log.Printf(" [x] Got:\n  - body: %s\n  - replyTo: %s\n  - route: %s", body, replyTo, route)
+			r := Response{Body: body, ReplyTo: replyTo, Route: route}
+			rbody, errMarshal := json.Marshal(r)
+			if errMarshal != nil {
+				log.Printf("Couldn't marshal response body")
+				return
+			}
+			log.Printf(" [x] prepared response: %s", rbody)
+			resp, err := http.Post(responseEndpoint, "application/json", bytes.NewBuffer(rbody))
+			if err != nil {
+				log.Printf("Couldn't send POST request to consumer")
+			} else {
+				log.Printf(" [x] Sent via REST: %s", resp.Status)
+			}
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
+}
